60015de43ded21f98cca965c1f38d0227cd7aae8
Madhur Jain <mjain.sps@gmail.com>
2021-05-14 22:41:05 +0530

    latest

--- Resolvers/index.js
+++ Resolvers/index.js
@@ -4,16 +4,49 @@ const testFunction = require("../Firebase/coverting.function");
 
 const resolvers = {
   Query: {
-    test: async () => {
-      const dataRef = db.collection("test");
-      const snapShot = await dataRef.get();
-      const data = snapShot.docs.map((ele) => ele.data());
+    //Resolves into collection ID and title only
+    collectionTitles: async () => {
+      const collectionRef = db.collection("collection");
+      const snapShot = await collectionRef.get();
+      const data = snapShot.docs.map((element) => {
+        const returnObject = {
+          id: element.id,
+          title: element.data().title,
+        };
+        return returnObject;
+      });
       return data;
     },
 
-    collection: () => {
-      const returnObject = { id: "1", title: "Madhur Jain" };
-      return returnObject;
+    //Resolves into collection ID and title and items array also.
+    collection: async () => {
+      const collectionRef = db.collection("collection");
+      const snapShot = await collectionRef.get();
+
+      const data = snapShot.docs.map((element) => {
+        console.log(element);
+        const returnObject = {
+          id: element.id,
+          title: element.data().title,
+          items: element.data().items,
+        };
+        return returnObject;
+      });
+      return data;
+    },
+
+    users: async () => {
+      const userRef = db.collection("users");
+      const snapShot = await userRef.get();
+      const data = snapShot.docs.map((element) => {
+        const returnObject = {
+          id: element.id,
+          displayName: element.data().displayName,
+          email: element.data().email,
+        };
+        return returnObject;
+      });
+      return data;
     },
   },
 };
--- TypeDefs/index.js
+++ TypeDefs/index.js
@@ -1,17 +1,36 @@
 const { gql } = require("apollo-server");
 
 const typeDefs = gql`
-  type Test {
-    name: String
-  }
-  type Collection {
+  #To get collection id and its title only
+  type OnlyCollectionTitles {
     id: ID
     title: String
   }
 
+  #Types to get collection with all items
+  type CollectionWithItems {
+    id: ID
+    title: String
+    items: [Items]
+  }
+  type Items {
+    id: String
+    imageUrl: String
+    name: String
+    price: Int
+  }
+
+  #to get usersData
+  type Users {
+    id: ID
+    displayName: String
+    email: String
+  }
+
   type Query {
-    test: [Test]
-    collection: [Collection]
+    collectionTitles: [OnlyCollectionTitles]
+    collection: [CollectionWithItems]
+    users: [Users]
   }
 `;
 
